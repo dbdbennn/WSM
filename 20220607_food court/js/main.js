cbd5eb24d5242ff33acf7b54694edd41fd2dae17
Jung Eun <0hh.0553@gmail.com>
2022-06-14 14:59:24 +0900

    js 여러가지 함수 모습 복습

---
+++ 20220607_food court/js/main.js
@@ -0,0 +1,11 @@
+/* Function */
+// function say() {
+//     console.log("Hello World");
+// }
+
+// const say = function(){
+//     console.log("Hello World2");
+// }
+
+// const say = () => console.log("Hello World3");
+// say();
