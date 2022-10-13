70d8cdfc75b34ad99073f8378c830832d93ff5d8
Jung Eun <0hh.0553@gmail.com>
2022-10-13 15:08:34 +0900

    mouseover -> 그 날짜 url

--- 20220901_grid/js/openapi.js
+++ 20220901_grid/js/openapi.js
@@ -12,17 +12,55 @@
 // MLSV_TO_YMD : 급식종료일자
 
 // https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010569&MMEAL_SC_CODE=2&MLSV_YMD=20221011&Type=json&KEY=a9c902f701a34fe494d123a5b4970beb
-const KEY = "a9c902f701a34fe494d123a5b4970beb";
-const ATPT_OFCDC_SC_CODE = "B10";   // 서울특별시교육청
-const SD_SCHUL_CODE = "7010569";    // 미림여자정보과학고등학교
-let MMEAL_SC_CODE = 2;              // 중식
-let MLSV_YMD = "20221011";          // YYYYMMDD
-const Type = "json";
-let url = `https://open.neis.go.kr/hub/mealServiceDietInfo?`+
-`&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`+
-`&SD_SCHUL_CODE=${SD_SCHUL_CODE}`+
-`&MMEAL_SC_CODE=${MMEAL_SC_CODE}`+
-`&MLSV_YMD=${MLSV_YMD}`+
-`&Type=json`+
-`&KEY=${KEY}`;
-console.log(url);
+// const KEY = "a9c902f701a34fe494d123a5b4970beb";
+// const ATPT_OFCDC_SC_CODE = "B10";   // 서울특별시교육청
+// const SD_SCHUL_CODE = "7010569";    // 미림여자정보과학고등학교
+// let MMEAL_SC_CODE = 2;              // 중식
+// let MLSV_YMD = "20221011";          // YYYYMMDD
+// const Type = "json";
+// let url = `https://open.neis.go.kr/hub/mealServiceDietInfo?`+
+// `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`+
+// `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`+
+// `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`+
+// `&MLSV_YMD=${MLSV_YMD}`+
+// `&Type=json`+
+// `&KEY=${KEY}`;
+
+// // console.log(url);
+
+// 실시간으로 급식 메뉴 가져오자
+// .date-grid-container>.grid-item에 마우스 올려놓으면, handler 함수 호출하자
+let dateGridConatinerDiv = document.getElementsByClassName("date-grid-container")[0];
+let gridItems = dateGridConatinerDiv.getElementsByClassName("grid-item"); // 모든 클래스를 가져옴
+const handler = (e) => {
+    console.log(year);
+    console.log(month);
+    let date = e.target.innerHTML; // 이벤트가 일어난 해당 텍스트를 가져옴
+    console.log(date);
+    // handler에서 year, month, date, 식사 로 url 만들어서 AJAX로 급식 정보 가져오자
+
+    const KEY = "a9c902f701a34fe494d123a5b4970beb";
+    const ATPT_OFCDC_SC_CODE = "B10";   // 서울특별시교육청
+    const SD_SCHUL_CODE = "7010569";    // 미림여자정보과학고등학교
+    // let MMEAL_SC_CODE = 2;              // 중식
+    // zfill
+    let MLSV_YMD = `${year}${month.toString().padStart(2, "0")}${date.padStart(2, "0")}`;          // YYYYMMDD
+    console.log(MLSV_YMD);
+    const Type = "json";
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`+
+        `?ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`+
+        `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`+
+        // `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`+ 조중석 합쳐서 나옴
+        `&MLSV_YMD=${MLSV_YMD}`+
+        `&Type=json`+
+        `&KEY=${KEY}`;
+
+    console.log(url);
+
+    urlToJSON(url);
+}
+for (let gridItem of gridItems) {
+    gridItem.onmouseover = handler;
+    // gridItem.addEventListener("mouseover", handler);
+}
+// 다 가져왔으면, 조식, 중식, 석식 표시하자
