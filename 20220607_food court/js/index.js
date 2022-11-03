b042db934f115988b022e1e5742bf236454a3921
Jung Eun <0hh.0553@gmail.com>
2022-11-03 15:10:39 +0900

    js 오늘의 메뉴 급식 API에서 가져와서 HTML에 표시하자

--- 20220607_food court/js/index.js
+++ 20220607_food court/js/index.js
@@ -99,6 +99,87 @@ showToday();
 
 // 오늘의 급식 가져오고, 표시하자
 const showTodayMenu = () => {
-    //
+    // 지금 구하자
+    let now = new Date();
+
+    // 년, 월, 일 구하자
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let date = now.getDate(); 
+
+    // 급식 API 요청할 url 만들자
+    const KEY = "a9c902f701a34fe494d123a5b4970beb";
+    const ATPT_OFCDC_SC_CODE = "B10";   // 서울특별시교육청
+    const SD_SCHUL_CODE = "7010569";    // 미림여자정보과학고등학교
+    // let MMEAL_SC_CODE = 2;              // 중식
+    // zfill
+    let MLSV_YMD = `${year}${month.toString().padStart(2, "0")}${date.toString().padStart(2, "0")}`;          // YYYYMMDD
+    console.log(MLSV_YMD);
+    const Type = "json";
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`+
+        `?ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`+
+        `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`+
+        // `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`+ 조중석 합쳐서 나옴
+        `&MLSV_YMD=${MLSV_YMD}`+
+        `&Type=${Type}`+
+        `&KEY=${KEY}`;
+
+    console.log(url);
+    // 요청하자
+    fetch(url)
+    .then(response => response.json()) // 응답 온 데이터 -> json
+    .then(json => showMenu(json)); // json -> HTML에 표시하자
+
 };
+
+const showMenu = (json) => {
+  // HTML -> js 메뉴 표시하는 부분
+  let menus = document.querySelectorAll(".card-menu");
+  let breakfast = menus[0];
+  let lunch = menus[1];
+  let dinner = menus[2];
+
+  // json 안에 조식, 중식, 석식 정보 빼오고
+  try{
+    if (json['mealServiceDietInfo'][0]['head'][1]['RESULT']['CODE']=='INFO-000') { //응답이 제대로 왔으면
+        // json -> HTML
+        try{
+            let breakfastData = json['mealServiceDietInfo'][1]['row'][0]['DDISH_NM']; 
+            breakfast.innerHTML = breakfastData.replace(/\([0-9\.]*\)/g, ""); // 정규표현식 : (문자 숫자나 .문자)문자
+            // (            \(
+            // 숫자 한글자 [0123456789]
+            //  .           \.
+            // 0 ~ n개
+            // )            \)
+            // 글로벌        g
+        }catch{
+            breakfast.innerHTML = "없음"
+        }
+
+        try{
+            let lunchData = json['mealServiceDietInfo'][1]['row'][1]['DDISH_NM'];
+            lunch.innerHTML = lunchData.replace(/\([0-9\.]*\)/g, ""); 
+        }catch{
+            lunch.innerHTML = "없음"
+        }
+
+        try{
+            let dinnerData = json['mealServiceDietInfo'][1]['row'][2]['DDISH_NM'];
+            dinner.innerHTML = dinnerData.replace(/\([0-9\.]*\)/g, "");
+        }catch{
+            dinner.innerHTML = "없음"
+        }
+        
+    } else { // 응답이 이상하면
+        // 없음 표시하자
+        breakfast.innerHTML = "없음";
+        lunch.innerHTML = "없음";
+        dinner.innerHTML = "없음";
+    }
+  } catch { // 문제가 생기면 {'mealServiceDietInfo'} 아니고 {'RESULT'}로 옴
+      breakfast.innerHTML = "없음";
+      lunch.innerHTML = "없음";
+      dinner.innerHTML = "없음";
+  }
+}
 showTodayMenu();
